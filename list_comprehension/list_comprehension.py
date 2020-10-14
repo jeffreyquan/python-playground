270d2c8ffc730f88799556a741d7b2cc77900e6b
Jeffrey Quan <jeffrey.quan@hotmail.com>
2020-10-15 01:04:09 +1100

    basic list comprehension

---
+++ list_comprehension/list_comprehension.py
@@ -0,0 +1,16 @@
+letters = [letter for letter in 'comprehension']
+
+print(letters) # ['c','o','m','p','r','e','h','e','n','s','i','o','n']
+
+even_num_list = [x for x in range(10) if x % 2 == 0]
+
+print(even_num_list) # [0, 2, 4, 6, 8]
+
+num_list = [y for y in range(100) if y % 3 == 0 and y % 5 == 0]
+
+print(num_list) # [0, 15 , 30, 45, 60, 75, 90]
+
+names = "  Ben | John | Bob   | Cat  | Dog "
+name_list = [x.strip() for x in names.split("|")]
+
+print(name_list) # ['Ben', 'John', 'Bob', 'Cat', 'Dog']
